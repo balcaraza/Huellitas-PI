0b68332a0d1f4560527ade61c4f15b901c64a690
caropg <carolinapinedagrifaldo@gmail.com>
2024-03-08 00:00:05 -0600

    feat | Se comenzó a implementar la función en  para agregar productos a favoritos al seleccionar el corazón de las cards  almacenados en , y al mismo tiempo se aplican estilos específicos para destacar los productos favoritos en .

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,19 +1,24 @@
 
 let itemsContainer = document.getElementById("list-items");
 
+//Arreglo de los productos favoritos
+let favoritos = [] 
+
 let productos = [
   {
+    id: "1",
     img: "./src/img/Productos/RopaMinie.jpg",
     description: "Ropa para mascota, disney minnie mouse, camisa textil, rosa y grande",
     precio: "145.80",
   },
   {
+    id: "2",
     img: "./src/img/Productos/CepilloLimpieza.jpg",
     description: "Cepillo de limpieza para mascotas sintético azul 19.7x10.6x5 Cm",
     precio: "79.90",
   },
 
-  { img: "./src/img/Productos/TazonPanda.jpg", description: "Tazón para mascota, panda animal de plástico y color blanco", precio: "$99.90" },
+  { id: "3", img: "./src/img/Productos/TazonPanda.jpg", description: "Tazón para mascota, panda animal de plástico y color blanco", precio: "$99.90" },
   {
     img: "./src/img/Productos/PelucheDinosaurio.jpg",
     description: "Peluche para mascota de dinosaurio, 100% Poliéster 35.5x13 Cm",
@@ -67,11 +72,12 @@ localStorage.setItem("productos", productosJSON);
 productos.forEach(function (item) {
   // Crear el HTML para cada elemento
   let itemHTML = `
-        <div class="card">
+        <div class="card" id="${item.id}">
             <div>
                 <img style="max-height:300px" src="${item.img}" class="card-img-top" alt="...">
                 <button class="button-favorite">
-                    <i class="fa-regular fa-heart"></i>
+                <i class="fa-regular fa-heart" id="corazon-vacio"></i>
+                <i class="fa-solid fa-heart" id="corazon-lleno"></i>
                 </button> 
                 <img class="img-carrito" src="./src/img/CarritoCompras.png" alt="Carrito Compras">
             </div>
@@ -108,14 +114,31 @@ let nuevoItemHTML = `
     
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Obtener el botón de favoritos por su clase
-  const favoriteButton = document.querySelector(".button-favorite");
+//Actualizar los favoritos en el localStorage
+const actualizarFavoritos = () => {
+  localStorage.setItem("favoritos", JSON.stringify(favoritos));
+};
 
-  // Agregar un event listener para el evento de clic
-  favoriteButton.addEventListener("click", function () {
-    // Alternar la clase clicked al botón de favoritos
-    favoriteButton.classList.toggle("clicked");
+//Cargar los favoritos
+const cargaFavoritos = () => {
+  const almacenarFavoritos = localStorage.getItem("favoritos");
+
+  if(almacenarFavoritos){
+    favoritos = JSON.parse(almacenarFavoritos);
+  }
+};
+
+//Agrega un evento que escucha el evento DOMContentLoaded al objeto document. El DOMContentLoaded se dispara cuando el HTML ha sido completamente cargado y analizado
+document.addEventListener("DOMContentLoaded", function() {
+  //Se seleccionan todos los elementos del DOM que tienen la clase .button-favorite y se almacenan en la variable btnFavorite
+  const btnFavorite = document.querySelectorAll(".button-favorite");
+  //Iterar sobre cada botón en la colección btnFavorite
+  btnFavorite.forEach(boton => {
+    boton.addEventListener('click', () => {
+      const card = e.target.closest(".card");
+      
+
+    });
   });
 });
 
