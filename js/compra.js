a6105877e5a9b6bb43eb7783ca7d21b3816526c2
Balam <balcaraza@gmail.com>
2024-03-12 23:24:07 -0600

    Feature| se agrega formulario de compra (direccion y metodo de pago)

--- js/compra.js
+++ js/compra.js
@@ -1,3 +1,17 @@
+var cerrar=document.getElementById('cerrar');
+var cerrar2=document.getElementById('cancelar');
+var tarjeta=document.getElementById("tarjeta");
+
+//mostrar tarjeta
 document.getElementById("mostrarTarjeta").addEventListener("click", function() {
     document.getElementById("tarjeta").style.display = "block";
 });
+
+//botones de cerrar tarjeta y cancelar
+cerrar.addEventListener("click", function(){
+   tarjeta.style.display = 'none'; 
+})
+cancelar.addEventListener("click", function(){
+   tarjeta.style.display = 'none'; 
+})
+
