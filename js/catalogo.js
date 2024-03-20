9bfa3dbdc2893967c6f2ebe19412675f17a13536
andriguo <andreacervantessosa@gmail.com>
2024-03-19 20:59:04 -0600

    Se borran avavances en categorias, pruebas cambios footer

--- js/catalogo.js
+++ js/catalogo.js
@@ -9,15 +9,13 @@ let productos = [
     id: "1",
     img: "./src/img/Productos/RopaMinie.jpg",
     description: "Ropa para mascota, disney minnie mouse, camisa textil, rosa y grande",
-    precio: "145.80",
-    categoria: "ropa",
+    precio: "145.80"
   },
   {
     id: "2",
     img: "./src/img/Productos/CepilloLimpieza.jpg",
     description: "Cepillo de limpieza para mascotas sintético azul 19.7x10.6x5 Cm",
-    precio: "79.90",
-    categoria: "accesorios",
+    precio: "79.90"
   },
 
   { id: "3", img: "./src/img/Productos/TazonPanda.jpg", description: "Tazón para mascota, panda animal de plástico y color blanco", precio: "99.90" },
@@ -25,64 +23,55 @@ let productos = [
     id: "4",
     img: "./src/img/Productos/PelucheDinosaurio.jpg",
     description: "Peluche para mascota de dinosaurio, 100% Poliéster 35.5x13 Cm",
-    precio: "129.90",
-    categoria: "juguete",
+    precio: "129.90"
   },
   {
     id: "5",
     img: "./src/img/Productos/JugueteFutbol.jpg",
     description: "Juguete para mascota con sonido",
-    precio: "29.90",
-    categoria: "juguete",
+    precio: "29.90"
   },
   {
     id: "6",
     img: "./src/img/Productos/toallitaAzul.jpg",
     description: "Toalla de baño con capucha, absorbente de alta calidad",
-    precio: "150",
-    categoria: "ropa",
+    precio: "150"
   },
   {
     id: "7",
     img: "./src/img/Productos/casaGatos.jpg",
     description: "Casa para gatos, color morada con rascadero",
-    precio: "350",
-    categoria: "muebles",
+    precio: "350"
   },
   {
     id: "8",
     img: "./src/img/Productos/ChalecoArnes.jpg",
     description: "Chaleco con arnés para mascotas 100% poliéster azul 25x35",
-    precio: "99.90",
-    categoria: "ropa",
+    precio: "99.90"
   },
   {
     id: "9",
     img: "./src/img/Productos/DisfrazLaGarra.jpg",
     description: "Disfraz para mascota, de disney alíen toy story, textil de color verde y grande",
-    precio: "149.90",
-    categoria: "ropa",
+    precio: "149.90"
   },
   {
     id: "10",
     img: "./src/img/Productos/TazonRosa.jpg",
     description: "Tazón para mascota de disney minnie mouse, plástico y de color rosa",
-    precio: "99.90",
-    categoria: "accesorios",
+    precio: "99.90"
   },
   {
     id: "11",
     img: "./src/img/Productos/cama.jpg",
     description: "Cama Resistente con Relleno Suave y Color cafe",
-    precio: "299",
-    categoria: "muebles",
+    precio: "299"
   },
   {
     id: "12",
     img: "./src/img/Productos/collarPerro.jpg",
     description: "Collar de piel, color cafe, grabado",
-    precio: "250",
-    categoria: "accesorios",
+    precio: "250"
   },
 ];
 let productosJSON = JSON.stringify(productos);
@@ -139,25 +128,6 @@ let nuevoItemHTML = `
   itemsContainer.insertAdjacentHTML("beforeend",nuevoItemHTML);
     
 });
-
-let btnRopa = document.getElementById("btnRopa");
-let btnMuebles = document.getElementById("btnMuebles");
-let btnJuguetes = document.getElementById("btnJuguetes");
-let btnAccesorios = document.getElementById("btnAccesorios");
-
-btnRopa.addEventListener("click", function (event) {
-  event.preventDefault();
-  const prodRopa = filtrarProdxCat(productos, "ropa");
-  console.log(prodRopa);
-});
-
-function filtrarProdxCat(productos, categoria) {
-  return productos.filter(product => product.categoria === categoria);
-  
-}
-productosJSON = JSON.stringify(productos);
-localStorage.setItem("productos", productosJSON);
-
 
 const producto = document.querySelectorAll(".card");
 
